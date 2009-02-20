9b5531cf553668afdb8b080449438a44d7adbb69
tuergeist <tuergeist@6e64f258-f361-0410-b52a-daece7a02511>
2009-02-20 12:17:40 +0000

    aeolbonn test fixed

--- ET/trunk/src/test/java/de/berlios/esotranslator/aeolbonn/AeolbonnParserTest.java
+++ ET/trunk/src/test/java/de/berlios/esotranslator/aeolbonn/AeolbonnParserTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import com.sun.xml.internal.fastinfoset.util.StringArray;
 
+import de.berlios.esotranslator.CodeContainer;
+
 public class AeolbonnParserTest {
 
 	@Test
@@ -27,7 +29,10 @@ public class AeolbonnParserTest {
 		writerMock.print("Hello World!");
 		replay(writerMock);
 
+		CodeContainer containerMock = createMock(JavaBuilder.class);
+		
 		AeolbonnParser ap = new AeolbonnParser();
+		ap.setContainer(containerMock);
 		ap.setWriter(writerMock);
 		StringArray program = new StringArray();
 		program.add(":Hello World!");
